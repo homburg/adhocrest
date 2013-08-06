2c45f0f2dacb64dd8b77f4276ce464db3726c661
Thomas B Homburg <thomas@homburg.dk>
2013-08-07 01:00:22 +0200

    Features

--- rest.go
+++ rest.go
@@ -1,65 +1,170 @@
-package main
+// Package for delivering a development rest server.
+package adhocrest
 
 import (
 	"strings"
+	"io/ioutil"
 	"net/http"
 	"log"
 	"runtime"
 	"fmt"
+	"encoding/json"
+
+	"github.com/nu7hatch/gouuid"
 )
 
 var address string
+
+// A resource is just json
+type resource string
+
+// Resources, indexed by uuid
+type resources map[string]resource
+
+// The global resource cache
+type resourceCache map[string]resources
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
-func main() {
-	address = "localhost:31415"
-
-	http.HandleFunc("/", index)
-
-	log.Printf("Starting server (%s)...\n", address)
-	http.ListenAndServe(address, nil)
-}
-
 var html string
 
 func respond(w http.ResponseWriter, r *http.Request) {
-	headers := []string{
-		"Allow",
-		"Access-Control-Allow-Methods",
-	}
-
-	for _, header := range headers {
-		w.Header().Set(header, "POST,PUT,DELETE,GET,OPTIONS")
-	}
-	w.Header().Set("Access-Control-Allow-Origins", "*")
 	fmt.Fprintln(w, r.Method)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+func serveError(w http.ResponseWriter, r *http.Request, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	data, _ := json.Marshal(map[string]string{"status": "500", "message": err.Error()})
+	w.Write(data)
+}
+
+// Get new handler with the given root
+func NewHandler(root string) *Handler {
+	return &Handler{root, resourceCache{}}
+}
+
+type Handler struct {
+	root string
+	db resourceCache
+}
+
+type route struct {
+	Resource, Id string
+}
+
+func getRoute(path string) route {
+	path = strings.TrimLeft(path, "/")
+	pathParts := strings.Split(path, "/")
+
+	if len(pathParts) == 1 {
+		return route{pathParts[0], ""}
+	}
+
+	if len(pathParts) >= 2 {
+		return route{pathParts[0], pathParts[1]}
+	}
+
+	return route{}
+}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	r.URL.Path = strings.TrimPrefix(r.URL.Path, h.root)
+	route := getRoute(r.URL.Path)
 	switch strings.ToUpper(r.Method) {
-		case "OPTIONS":
-			// Cross-origin post
-			log.Printf(r.Method)
-			respond(w, r)
 		case "POST":
-			// create
-			log.Printf(r.Method)
-			respond(w, r)
+			// Create!
+			// Return the location to the created resource
+
+			id, err := uuid.NewV4()
+			if nil != err {
+				serveError(w, r, err)
+				return
+			}
+			newResourceId := id.String()
+
+			// Create it, the resource list?
+			if _, ok := h.db[route.Resource]; !ok {
+				h.db[route.Resource] = make(resources)
+			}
+
+			bodyStr, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				serveError(w, r, err)
+				return
+			}
+
+			// Save resource
+			bodyData := make(map[string]string)
+			err = json.Unmarshal(bodyStr, &bodyData)
+			if nil != err {
+				serveError(w, r, err)
+				return
+			}
+			bodyData["id"] = newResourceId
+			dataStr, err := json.Marshal(bodyData)
+			if nil != err {
+				serveError(w, r, err)
+				return
+			}
+			h.db[route.Resource][newResourceId] = resource(dataStr)
+			if nil != err {
+				serveError(w, r, err)
+				return
+			}
+
+			w.Header().Set("location", "/" + route.Resource + "/" + newResourceId)
+			w.WriteHeader(http.StatusCreated)
 		case "PUT":
-			// update
+			// Update
 			log.Printf(r.Method)
 			respond(w, r)
 		case "DELETE":
-			log.Printf(r.Method)
-			respond(w, r)
-			// drop
+			// Drop
+			if _, ok := h.db[route.Resource]; !ok {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			if _, ok := h.db[route.Resource][route.Id]; ok {
+				delete(h.db[route.Resource], route.Id)
+				return
+			} else {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
 		case "GET":
-			// read
-			log.Printf(r.Method)
-			respond(w, r)
+			// Read
+			if "" == route.Id {
+				// List
+				jsonList := "["
+				if _, ok := h.db[route.Resource]; ok {
+					for _, jsonItem := range h.db[route.Resource] {
+						jsonList += string(jsonItem) + ","
+					}
+				}
+				jsonList = strings.TrimRight(jsonList, ",")
+				jsonList += "]"
+				w.Write([]byte(jsonList))
+			} else {
+				// Get
+				if _, ok := h.db[route.Resource]; !ok {
+					w.WriteHeader(http.StatusNotFound)
+					return
+				}
+
+				log.Println(h.db[route.Resource])
+
+				if resource, ok := h.db[route.Resource][route.Id]; ok {
+					w.Write([]byte(resource))
+					return
+				} else {
+					w.WriteHeader(http.StatusNotFound)
+					return
+				}
+			}
 		case "HEAD":
 			log.Println("Unsupported method")
 			w.WriteHeader(http.StatusMethodNotAllowed)
--- rest_test.go
+++ rest_test.go
@@ -1,10 +1,13 @@
-package main
+package adhocrest
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"strings"
+	"bytes"
 )
 
 func testMethod(method string, callback func (w *httptest.ResponseRecorder)) {
@@ -14,7 +17,9 @@ func testMethod(method string, callback func (w *httptest.ResponseRecorder)) {
 	}
 
 	w := httptest.NewRecorder()
-	index(w, req)
+
+	handler := NewHandler("")
+	handler.ServeHTTP(w, req)
 
 	callback(w)
 }
@@ -27,23 +32,168 @@ func TestInvalidMethod(t *testing.T) {
 	})
 }
 
-func TestGet(t *testing.T) {
-	testMethod("GET", func (w *httptest.ResponseRecorder) {
-		if w.Body.String() != "GET\n" {
-			t.Errorf("Expected body \"GET\", got\n-----\n%s\n----\n", string(w.Body.String()))
-		}
-	})
+func TestPostBadData(t *testing.T) {
+	handler := NewHandler("")
+
+	req, _ := http.NewRequest("POST", "/", strings.NewReader("[]"))
+	w := httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+
+	if http.StatusInternalServerError != w.Code {
+		t.Errorf(
+			"Expected internal server error on non-\"dict\" post, got response\n%s",
+			w.Body.String(),
+		)
+	}
+
+	req, _ = http.NewRequest("POST", "/", strings.NewReader("500"))
+	w = httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+
+	if http.StatusInternalServerError != w.Code {
+		t.Errorf(
+			"Expected internal server error on non-\"dict\" post, got response\n%s",
+			w.Body.String(),
+		)
+	}
+
+	req, _ = http.NewRequest("POST", "/", strings.NewReader(`{"from":"reader"}`))
+	w = httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+
+	if http.StatusCreated != w.Code {
+		t.Errorf("Expected %d on valid \"dict\" data, got %d", http.StatusCreated, w.Code)
+	}
 }
 
-func TestPostNewResource(t *testing.t) {
-	req, err := http.NewRequest("POST", "http://example.com/foo", nil)
-	req.Write
-	if err != nil {
-		log.Fatal(err)
+func TestPostNewResource(t *testing.T) {
+
+	handler := NewHandler("")
+
+	// Create a resource
+	data := map[string]string{"from":"httptest.Recorder"}
+	jsonData, err := json.Marshal(data)
+	if nil != err {
+		t.Error(err)
+	}
+
+	req, err := http.NewRequest("POST", "/fish", strings.NewReader(string(jsonData)))
+	if nil != err {
+		t.Error(err)
 	}
 
 	w := httptest.NewRecorder()
-	index(w, req)
+	handler.ServeHTTP(w, req)
 
-	callback(w)
+	// Create another
+	req, err = http.NewRequest("POST", "/fish", strings.NewReader(string(jsonData)))
+	if nil != err {
+		t.Error(err)
+	}
+	w = httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+
+	if nil != err {
+		t.Error(err)
+	}
+
+	if w.Code != http.StatusCreated {
+		t.Errorf("Expected status code %d, got %d", http.StatusCreated, w.Code)
+	}
+
+	headers := w.Header()
+	_, ok := headers["Location"]
+
+	var locationHeader string
+	if ok {
+		locationHeader = headers.Get("location")
+		expectedHeaderPrefix := "/fish/"
+		if !strings.HasPrefix(locationHeader, expectedHeaderPrefix) || len(locationHeader) < len(expectedHeaderPrefix) {
+			t.Errorf("Expected location to match /fish/<string:id>, got %s", locationHeader)
+		}
+
+	} else {
+		t.Errorf("Expected location header on create, got:\n%s", headers)
+	}
+
+	// Get the same info again
+	req, err = http.NewRequest("GET", locationHeader, nil)
+	if nil != err {
+		t.Error(err)
+	}
+
+	w = httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+	responseBodyStr := w.Body.String()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Could not get resource from %s, status: %d", locationHeader, w.Code)
+	}
+
+	// Unmarshal recieved json data, and remove id
+	responseData := make(map[string]string)
+	err = json.Unmarshal(w.Body.Bytes(), &responseData)
+	if nil != err {
+		t.Error(err)
+	}
+	delete(responseData, "id")
+	responseDataStr, err := json.Marshal(responseData)
+	if nil != err {
+		t.Error(err)
+	}
+
+	if 0 != bytes.Compare(jsonData, responseDataStr) {
+		t.Errorf(
+			`Expected to match posted data plus id:
+-----
+%s
+-----
+got
+-----
+%s
+-----`,
+			jsonData,
+			responseBodyStr,
+		)
+	}
+
+	// Get a list
+	req, err = http.NewRequest("GET", "/fish", nil)
+	if nil != err {
+		t.Error(err)
+	}
+
+	w = httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status 200 on resource listing, got %d", w.Code)
+	}
+
+	// Delete it
+	req, err = http.NewRequest("DELETE", locationHeader, nil)
+	if nil != err {
+		t.Error(err)
+	}
+
+	w = httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected http.StatusOK on DELETE, got %d", w.Code)
+	}
+
+	// Get it again (not)
+	req, err = http.NewRequest("GET", locationHeader, nil)
+	if nil != err {
+		t.Error(err)
+	}
+
+	w = httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+
+	if w.Code != http.StatusNotFound {
+		t.Errorf("Expected http status 404, Not Found, on %s, got %d. Was it not deleted?",
+			locationHeader, w.Code)
+	}
 }
